04e27cbbe86cfa6683ce02cd60fab9dc3e09c183
unilock <katherines1337@outlook.com>
2026-02-01 17:59:44 -0500

    only load KnickkacksClient on client

--- src/main/java/tech/thatgravyboat/knickknacks/client/KnickkacksClient.java
+++ src/main/java/tech/thatgravyboat/knickknacks/client/KnickkacksClient.java
@@ -1,12 +1,13 @@
 package tech.thatgravyboat.knickknacks.client;
 
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.common.NeoForge;
 import tech.thatgravyboat.knickknacks.Knickknacks;
 import tech.thatgravyboat.knickknacks.client.perks.LifeNeedleClient;
 import tech.thatgravyboat.knickknacks.client.perks.MinersGogglesClient;
 
-@Mod(Knickknacks.MODID)
+@Mod(value = Knickknacks.MODID, dist = Dist.CLIENT)
 public class KnickkacksClient {
 
     public KnickkacksClient() {
